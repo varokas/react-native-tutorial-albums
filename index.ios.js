b637e68f89ef3d309bb9fefd16769db673142486
Varokas Panusuwan <varokas@Vinais-Mac-mini.local>
2017-04-15 20:34:58 +0700

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
+
+import Header from './src/components/header'
+
+const App = () => {
+  return (
+    <Header></Header>
+  );
+}
+
+AppRegistry.registerComponent('albums', () => App);
